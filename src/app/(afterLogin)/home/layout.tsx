4c103108e0bdd26574bdbb4a35b12913e8e0a0d9
KIMCHOONGMAN <cm730712@gmail.com>
2024-06-21 15:49:39 +0900

    [Add] 로그인 전,후 페이지 생성

--- src/app/(afterLogin)/home/layout.tsx
+++ src/app/(afterLogin)/home/layout.tsx
@@ -1,3 +1,5 @@
-export default async function name({ children }) {
+import { ReactNode } from "react";
+
+export default async function name({ children }: { children: ReactNode }) {
   return <div>홈 레이아웃{children}</div>;
 }
