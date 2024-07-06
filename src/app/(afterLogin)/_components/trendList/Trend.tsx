f2fd92cdf50a7d15be4d7eebf410ea863629ed27
KIMCHOONGMAN <cm730712@gmail.com>
2024-07-06 13:46:44 +0900

    나를 위한 트렌드 ui

---
+++ src/app/(afterLogin)/_components/trendList/Trend.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import style from "./trend.module.css";
+
+export default function Trend() {
+  return (
+    <Link href={`/search?q=트렌드`} className={style.container}>
+      <div className={style.count}>Trending in South Korea</div>
+      <div className={style.title}>keem</div>
+      <div className={style.count}>10.1K posts</div>
+    </Link>
+  );
+}
--- src/app/(afterLogin)/_components/trendList/TrendList.tsx
+++ src/app/(afterLogin)/_components/trendList/TrendList.tsx
@@ -1,5 +1,18 @@
+import Trend from "./Trend";
 import style from "./trendList.module.css";
 
 export default function TrendList() {
-  return <div>TrendList</div>;
+  return (
+    <main className={style.container}>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
