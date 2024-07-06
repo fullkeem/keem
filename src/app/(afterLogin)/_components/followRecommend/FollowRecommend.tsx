7b00c51f53da3d91e678b7bb3a254669f9f2b3c5
KIMCHOONGMAN <cm730712@gmail.com>
2024-07-06 13:47:00 +0900

    팔로우 추천 ui

--- src/app/(afterLogin)/_components/followRecommend/FollowRecommend.tsx
+++ src/app/(afterLogin)/_components/followRecommend/FollowRecommend.tsx
@@ -1,5 +1,31 @@
+"use client";
+
+import Image from "next/image";
 import style from "./followRecommend.module.css";
 
 export default function FollowRecommend() {
-  return <div>FollowRecommend</div>;
+  const onFollow = () => {};
+
+  const user = {
+    id: "elonmusk",
+    nickname: "Elon Musk",
+    image: "/x-logo.jpg",
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.userLogoSection}>
+        <div className={style.userLogo}>
+          <Image src={user.image} alt={user.id} width={50} height={50} />
+        </div>
+      </div>
+      <div className={style.userInfo}>
+        <div className={style.title}>{user.nickname}</div>
+        <div className={style.count}>@{user.id}</div>
+      </div>
+      <div className={style.followButtonSection}>
+        <button onClick={onFollow}>팔로우</button>
+      </div>
+    </div>
+  );
 }
