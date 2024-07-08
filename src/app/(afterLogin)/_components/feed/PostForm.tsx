166962dccb93ca83dde033efd4a156a119d4b098
KIMCHOONGMAN <cm730712@gmail.com>
2024-07-08 23:57:10 +0900

    PostForm ui 구현

---
+++ src/app/(afterLogin)/_components/feed/PostForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState, useRef, ChangeEventHandler, FocusEventHandler } from "react";
+import Image from "next/image";
+import style from "./postForm.module.css";
+import userImg from "/public/KEEM_logo.png";
+
+export default function PostForm() {
+  const imageRef = useRef(null);
+  const me = {
+    id: "fullkeem",
+    image: userImg,
+  };
+  return (
+    <form className={style.postForm}>
+      <div className={style.postUserSection}>
+        <div className={style.postUserImage}>
+          <Image src={me.image} alt={me.id} width={40} height={40} />
+        </div>
+      </div>
+      <div className={style.postingSection}>
+        <div className={style.postInputSection}>
+          <label htmlFor="posting" />
+          <input
+            name="post up"
+            id="posting"
+            placeholder="What is happening?!"
+            className={style.input}
+          />
+        </div>
+        <div className={style.postButtonSection}>
+          <div>
+            <input type="file" name="imageFiles" multiple hidden />
+            <button type="button" className={style.uploadButton}>
+              <svg width={24} viewBox="0 0 24 24" aria-hidden="true">
+                <g>
+                  <path d="M3 5.5C3 4.119 4.119 3 5.5 3h13C19.881 3 21 4.119 21 5.5v13c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 21 3 19.881 3 18.5v-13zM5.5 5c-.276 0-.5.224-.5.5v9.086l3-3 3 3 5-5 3 3V5.5c0-.276-.224-.5-.5-.5h-13zM19 15.414l-3-3-5 5-3-3-3 3V18.5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-3.086zM9.75 7C8.784 7 8 7.784 8 8.75s.784 1.75 1.75 1.75 1.75-.784 1.75-1.75S10.716 7 9.75 7z"></path>
+                </g>
+              </svg>
+            </button>
+          </div>
+          <div>
+            <button type="submit" className={style.postButton} disabled>
+              post
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
