ab242d16e92475e07c7d28ffc409c6867fd4a9da
KIMCHOONGMAN <cm730712@gmail.com>
2024-07-25 16:59:42 +0900

    Tab 컴포넌트 explore 경로일 때 ui 추가

--- src/app/(afterLogin)/_components/homeTab/Tab.tsx
+++ src/app/(afterLogin)/_components/homeTab/Tab.tsx
@@ -9,6 +9,26 @@ export default function Tab() {
   const pathname = usePathname();
 
   if (pathname === "/explore") {
+    return (
+      <div className={style.exploreFixed}>
+        <ul className={style.homeTab}>
+          <li>
+            For you
+            <div
+              className={style.tabIndicator}
+              hidden={tab === "Following"}
+            ></div>
+          </li>
+          <li>
+            Trending
+            <div
+              className={style.tabIndicator}
+              hidden={tab === "For you"}
+            ></div>
+          </li>
+        </ul>
+      </div>
+    );
   }
 
   const onClickFol = () => {
