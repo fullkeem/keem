3cca9c77bb51ead0bc4af066687b795436c4bc4a
Choongman Kim <cm730712@gmail.com>
2024-09-18 23:35:40 +0900

    Home 페이지 UI 수정

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,19 +2,25 @@ import Post from "../_components/feed/Post";
 import PostForm from "../_components/postForm/PostForm";
 import Tab from "../_components/homeTab/Tab";
 import style from "./home.module.css";
+import RightSection from "../_components/RightSection/RightSection";
 
 export default function Home() {
   return (
-    <div className={style.home}>
-      <Tab />
-      <PostForm />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+    <div className={style.container}>
+      <div className={style.home}>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+      <div className={style.rightSection}>
+        <RightSection />
+      </div>
     </div>
   );
 }
