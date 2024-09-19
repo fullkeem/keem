05a2452dbbf832f0cdbd191b1ba316b62f779660
Choongman Kim <cm730712@gmail.com>
2024-09-19 23:08:56 +0900

    중첩 레이아웃 사용을 위해 현재 레이아웃 작업 중단점

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -2,12 +2,18 @@ import style from "./explore.module.css";
 import ExploreSearchBar from "./_components/ExploreSearchBar";
 import ExploreTrendList from "./_components/ExploreTrendList";
 import ExploreHeader from "./_components/ExploreHeader";
+import FollowRecommend from "../_components/followRecommend/FollowRecommend";
 
 export default function Explore() {
   return (
     <div className={style.container}>
-      <ExploreHeader />
-      <ExploreTrendList />
+      <div className={style.main}>
+        <ExploreHeader />
+        <ExploreTrendList />
+      </div>
+      <div className={style.follow}>
+        <FollowRecommend />
+      </div>
     </div>
   );
 }
